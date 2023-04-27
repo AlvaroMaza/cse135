691ffb403220f4b0031c7e6d22f70de74f55ee01
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-27 13:42:36 -0700

    updates

--- cgi-bin/php-post-echo.php
+++ cgi-bin/php-post-echo.php
@@ -1,25 +1,30 @@
 <?php
+// print HTTP headers
 header("Cache-Control: no-cache");
 header("Content-type: text/html");
 
-echo <<< END
+// print HTML file top
+echo <<<END
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Environment Variables</title>
+  <title>General Request Echo</title>
 </head>
 <body>
-	<h1 align="center">Environment Variables</h1>
-	<hr>
+  <h1 align="center">General Request Echo</h1>
+  <hr>
 END;
 
-// Loop over the environment variables and print each variable and its value
-foreach ($_SERVER as $key => $value) {
-  echo "<b>$key:</b> $value<br />\n";
-}
+// HTTP Protocol, HTTP Method, and the Query String are all available as superglobals
+echo "<p><b>HTTP Protocol:</b> {$_SERVER['SERVER_PROTOCOL']}</p>";
+echo "<p><b>HTTP Method:</b> {$_SERVER['REQUEST_METHOD']}</p>";
+echo "<p><b>Query String:</b> {$_SERVER['QUERY_STRING']}</p>";
 
-echo <<< END
-</body>
-</html>
-END;
+// Read the message body
+$form_data = file_get_contents('php://input');
+
+echo "<p><b>Message Body:</b> $form_data</p>";
+
+// Print the HTML file bottom
+echo "</body></html>";
 ?>

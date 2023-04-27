26734862a130ad32b512b9d4219adfa6d5c64803
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-27 13:48:14 -0700

    Update php-post-echo.php

--- cgi-bin/php-post-echo.php
+++ cgi-bin/php-post-echo.php
@@ -1,7 +1,6 @@
 <?php
-echo "Cache-Control: no-cache\n";
-echo "Content-type: text/html \n\n";
-
+header("Cache-Control: no-cache");
+header("Content-type: text/html");
 // print HTML file top
 echo <<<END
 <!DOCTYPE html>

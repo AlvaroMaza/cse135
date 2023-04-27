37d672e83a0cc99b163fc83175a40c184562c369
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-27 13:45:53 -0700

    Update php-post-echo.php

--- cgi-bin/php-post-echo.php
+++ cgi-bin/php-post-echo.php
@@ -1,30 +1,34 @@
 <?php
-// print HTTP headers
-header("Cache-Control: no-cache");
-header("Content-type: text/html");
+echo "Cache-Control: no-cache\n";
+echo "Content-type: text/html \n\n";
 
 // print HTML file top
 echo <<<END
 <!DOCTYPE html>
-<html>
-<head>
-  <title>General Request Echo</title>
-</head>
-<body>
-  <h1 align="center">General Request Echo</h1>
-  <hr>
+<html><head><title>POST Request Echo</title>
+</head><body><h1 align="center">POST Request Echo</h1>
+<hr>
 END;
 
-// HTTP Protocol, HTTP Method, and the Query String are all available as superglobals
-echo "<p><b>HTTP Protocol:</b> {$_SERVER['SERVER_PROTOCOL']}</p>";
-echo "<p><b>HTTP Method:</b> {$_SERVER['REQUEST_METHOD']}</p>";
-echo "<p><b>Query String:</b> {$_SERVER['QUERY_STRING']}</p>";
-
-// Read the message body
+// read in post data
 $form_data = file_get_contents('php://input');
 
-echo "<p><b>Message Body:</b> $form_data</p>";
+// parse the post data
+parse_str($form_data, $in);
 
+echo "<b>Message Body:</b><br />\n";
+echo "<ul>\n";
+
+// print out the post data
+$loop = 0;
+foreach ($in as $key => $value) {
+  $loop += 1;
+  if ($loop % 2 != 0) {
+    echo "<li>$key = $value</li>\n";
+  }
+}
+
+echo "</ul>\n";
 // Print the HTML file bottom
-echo "</body></html>";
+echo "</body></html>\n";
 ?>

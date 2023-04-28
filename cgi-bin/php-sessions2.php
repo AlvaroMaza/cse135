d470517d14202ad207676f28bf90f7f3a259eb5b
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-28 15:49:32 -0700

    Update php-sessions2.php

--- cgi-bin/php-sessions2.php
+++ cgi-bin/php-sessions2.php
@@ -1,39 +1,35 @@
 <?php
 // Headers
 header("Cache-Control: no-cache");
-header("Content-type: text/html\n\n");
+header("Content-type: text/html");
 
 // Body - HTML
 echo "<html>";
-echo "<head><title>PHP Sessions</title></head>\n";
+echo "<head><title>C Sessions</title></head>\n";
 echo "<body>";
-echo "<h1>PHP Sessions Page 2</h1>";
+echo "<h1>C Sessions Page 2</h1>";
 echo "<table>";
 
-if (isset($_COOKIE['CGISESSID']) && $_COOKIE['CGISESSID'] !== 'destroyed')
-{
-  echo "<tr><td>Cookie:</td><td>".$_COOKIE['CGISESSID']."</td></tr>\n";
-}
-else
-{
-  echo "<tr><td>Cookie:</td><td>None</td></tr>\n";
+if (isset($_COOKIE['CGISESSID']) && $_COOKIE['CGISESSID'] !== "destroyed") {
+    echo "<tr><td>Cookie:</td><td>" . $_COOKIE['CGISESSID'] . "</td></tr>\n";
+} else {
+    echo "<tr><td>Cookie:</td><td>None</td></tr>\n";
 }
 
 echo "</table>";
 
 // Links for other pages
 echo "<br />";
-echo "<a href=\"/cgi-bin/php-sessions1.php\">Session Page 1</a>";
+echo "<a href=\"/cgi-bin/c-sessions-1.cgi\">Session Page 1</a>";
 echo "<br />";
-echo "<a href=\"/php-cgiform.html\">C CGI Form</a>";
+echo "<a href=\"/c-cgiform.html\">C CGI Form</a>";
 echo "<br /><br />";
 
 // Destroy Cookie button
-echo "<form action=\"/cgi-bin/php-destroy-session.php\" method=\"get\">";
+echo "<form action=\"/cgi-bin/c-destroy-session.cgi\" method=\"get\">";
 echo "<button type=\"submit\">Destroy Session</button>";
 echo "</form>";
 
 echo "</body>";
 echo "</html>";
-return 0;
 ?>

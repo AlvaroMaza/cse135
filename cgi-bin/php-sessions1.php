02af64033579309ccc0b42a94f31d57fa2f3b0f5
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-28 16:19:17 -0700

    Update php-sessions1.php

--- cgi-bin/php-sessions1.php
+++ cgi-bin/php-sessions1.php
@@ -39,7 +39,7 @@ echo "</table>";
 
 // Links for other pages
 echo "<br />";
-echo "<a href=\"/php-sessions2.php\">Session Page 2</a>";
+echo "<a href=\"/cgi-bin/php-sessions2.php\">Session Page 2</a>";
 echo "<br />";
 echo "<a href=\"/php-cgiform.html\">PHP CGI Form</a>";
 echo "<br /><br />";

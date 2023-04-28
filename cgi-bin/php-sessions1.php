960ed08bafdbee634b464821b6afffffc17bbb8a
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-28 15:23:39 -0700

    Update php-sessions1.php

--- cgi-bin/php-sessions1.php
+++ cgi-bin/php-sessions1.php
@@ -1,46 +1,60 @@
 <?php
-// Start PHP Session
-session_start();
+// Headers
+header("Cache-Control: no-cache");
 
-// Include necessary libraries
-require_once('CGI.php');
-require_once('CGI/Session.php');
+// Get Name from Environment
+$username = fgets(STDIN);
 
-// Create a new PHP Session
-$session = new CGI_Session("driver:File", null, array('Directory'=>"/tmp"));
-
-// Create CGI Object
-$cgi = new CGI();
-
-// Create a new Cookie from the Session ID
-$cookie = $cgi->cookie(array('name'=>'CGISESSID', 'value'=>$session->id));
-
-// Send the Cookie in the HTTP response header
-header('Set-Cookie: ' . $cookie->as_string());
-
-//Store Data in the PHP Session
-$name = $session->param('username') ?: $cgi->param('username');
-$session->param("username", $name);
-
-// Output HTML
-echo "<html>";
-echo "<head>";
-echo "<title>PHP Sessions</title>";
-echo "</head>";
-echo "<body>";
-
-echo "<h1>PHP Sessions Page 1</h1>";
-
-if ($name){
-    echo "<p><b>Name:</b> $name";
-}else{
-    echo "<p><b>Name:</b> You do not have a name set</p>";
+// Check to see if a proper name was sent
+$name = "";
+if ($username[0] == 'u')
+{
+  $name = substr($username, 9);
 }
 
-echo "<br/><br/>";
-echo "<a href=\"/cgi-bin/php-sessions-2.php\">Session Page 2</a><br/>";
-echo "<a href=\"/php-cgiform.html\">PHP CGI Form</a><br />";
-echo "<form style=\"margin-top:30px\" action=\"/cgi-bin/php-destroy-session.php\" method=\"get\">";
+// Set the cookie using a header, add extra \n to end headers
+if (strlen($name) > 0)
+{
+  header("Content-type: text/html");
+  setcookie("CSID", $name);
+}
+else
+{
+  header("Content-type: text/html");
+}
+
+// Body - HTML
+echo "<html>";
+echo "<head><title>C Sessions</title></head>";
+echo "<body>";
+echo "<h1>C Sessions Page 1</h1>";
+echo "<table>";
+
+// First check for new Cookie, then Check for old Cookie
+if (strlen($name) > 0)
+{
+  echo "<tr><td>Cookie:</td><td>$name</td></tr>";
+}
+elseif (isset($_COOKIE["CSID"]) && $_COOKIE["CSID"] !== "destroyed")
+{
+  echo "<tr><td>Cookie:</td><td>" . $_COOKIE["CSID"] . "</td></tr>";
+}
+else
+{
+  echo "<tr><td>Cookie:</td><td>None</td></tr>";
+}
+
+echo "</table>";
+
+// Links for other pages
+echo "<br />";
+echo "<a href=\"/cgi-bin/c-sessions-2.cgi\">Session Page 2</a>";
+echo "<br />";
+echo "<a href=\"/c-cgiform.html\">C CGI Form</a>";
+echo "<br /><br />";
+
+// Destroy Cookie button
+echo "<form action=\"/cgi-bin/c-destroy-session.cgi\" method=\"get\">";
 echo "<button type=\"submit\">Destroy Session</button>";
 echo "</form>";
 

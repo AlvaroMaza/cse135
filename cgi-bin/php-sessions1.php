a799c27c1f6511a94b6216510d12ee67cf0144a1
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-28 15:29:07 -0700

    Update php-sessions1.php

--- cgi-bin/php-sessions1.php
+++ cgi-bin/php-sessions1.php
@@ -3,58 +3,51 @@
 header("Cache-Control: no-cache");
 
 // Get Name from Environment
-$username = fgets(STDIN);
+$stdin = fopen("php://stdin", "r");
+$username = fgets($stdin, 1000);
+fclose($stdin);
 
 // Check to see if a proper name was sent
 $name = "";
-if ($username[0] == 'u')
-{
-  $name = substr($username, 9);
+if (strpos($username, "username=") !== false) {
+    $name = substr($username, 9);
 }
 
 // Set the cookie using a header, add extra \n to end headers
-if (strlen($name) > 0)
-{
-  header("Content-type: text/html");
-  setcookie("CSID", $name);
-}
-else
-{
-  header("Content-type: text/html");
+if (strlen($name) > 0) {
+    header("Content-type: text/html");
+    header("Set-Cookie: $name\n");
+} else {
+    header("Content-type: text/html\n");
 }
 
 // Body - HTML
 echo "<html>";
-echo "<head><title>C Sessions</title></head>";
+echo "<head><title>PHP Sessions</title></head>\n";
 echo "<body>";
-echo "<h1>C Sessions Page 1</h1>";
+echo "<h1>PHP Sessions Page 1</h1>";
 echo "<table>";
 
 // First check for new Cookie, then Check for old Cookie
-if (strlen($name) > 0)
-{
-  echo "<tr><td>Cookie:</td><td>$name</td></tr>";
-}
-elseif (isset($_COOKIE["CSID"]) && $_COOKIE["CSID"] !== "destroyed")
-{
-  echo "<tr><td>Cookie:</td><td>" . $_COOKIE["CSID"] . "</td></tr>";
-}
-else
-{
-  echo "<tr><td>Cookie:</td><td>None</td></tr>";
+if (strlen($name) > 0) {
+    echo "<tr><td>Cookie:</td><td>$name</td></tr>\n";
+} else if (isset($_COOKIE['PHPSESSID'])) {
+    echo "<tr><td>Cookie:</td><td>" . $_COOKIE['PHPSESSID'] . "</td></tr>\n";
+} else {
+    echo "<tr><td>Cookie:</td><td>None</td></tr>\n";
 }
 
 echo "</table>";
 
 // Links for other pages
 echo "<br />";
-echo "<a href=\"/cgi-bin/c-sessions-2.cgi\">Session Page 2</a>";
+echo "<a href=\"/php-sessions-2.php\">Session Page 2</a>";
 echo "<br />";
-echo "<a href=\"/c-cgiform.html\">C CGI Form</a>";
+echo "<a href=\"/php-cgiform.html\">PHP CGI Form</a>";
 echo "<br /><br />";
 
 // Destroy Cookie button
-echo "<form action=\"/cgi-bin/c-destroy-session.cgi\" method=\"get\">";
+echo "<form action=\"/php-destroy-session.php\" method=\"get\">";
 echo "<button type=\"submit\">Destroy Session</button>";
 echo "</form>";
 

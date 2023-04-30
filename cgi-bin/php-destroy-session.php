a9247ddf5e2d09e44600d93ce1ca873aba5adfb4
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-30 13:03:41 -0700

    Update php-destroy-session.php

--- cgi-bin/php-destroy-session.php
+++ cgi-bin/php-destroy-session.php
@@ -19,8 +19,8 @@ echo '</head>';
 echo '<body>';
 echo '<h1>Session Destroyed</h1>';
 echo '<a href="/php-cgiform.php">Back to the PHP CGI Form</a><br />';
-echo '<a href="/cgi-bin/php-sessions-1.php">Back to Page 1</a><br />';
-echo '<a href="/cgi-bin/php-sessions-2.php">Back to Page 2</a>';
+echo '<a href="/cgi-bin/php-sessions1.php">Back to Page 1</a><br />';
+echo '<a href="/cgi-bin/php-sessions2.php">Back to Page 2</a>';
 echo '</body>';
 echo '</html>';
 ?>

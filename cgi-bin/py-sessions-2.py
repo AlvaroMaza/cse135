93aced99df76d68f3402458a6bbbffccf6c22b2c
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-04-30 22:59:54 -0700

    Update py-sessions-2.py

--- cgi-bin/py-sessions-2.py
+++ cgi-bin/py-sessions-2.py
@@ -39,7 +39,9 @@ body += '<table>'
 
 # Display stored data
 if 'username' in session_data:
-    body += '<tr><td>Session Data:</td><td>' + session_id + '</td></tr>\n'
+    body += '<tr><td>Session Data:</td><td>' + session_data['username'] + '</td></tr>\n'
+elif session_id:
+    body += '<tr><td>Session Data:</td><td>'+session_id+'</td></tr>\n'
 else:
     body += '<tr><td>Session Data:</td><td>None</td></tr>\n'
 

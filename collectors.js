164bbc5781ac277c67396f84f58eb441a0592fb7
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-16 15:54:23 -0700

    intro to collector

---
+++ collectors.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded',init);
+function init(){
+    let payload = {};
+
+    payload["url"] = window.location.href;
+    payload["referrer"] = document.referrer;
+
+    payload["timestamp"] = new Date().toISOString();
+
+    payload["userAgent"] = navigator.userAgent;
+    payload["screenDimensions"] = {
+        width: window.screen.width,
+        height: window.screen.height
+    };
+
+    navigator.sendBeacon("http://cse135spain.site/json", JSON.stringify(payload));
+}

368e0da2e69fc9d570e6d4537ca88c0120f1693e
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-17 09:17:48 -0700

    Update collector.js

--- collector.js
+++ collector.js
@@ -1,16 +1,33 @@
-window.addEventListener('DOMContentLoaded',init);
-function init(){
-    let payload = {};
+window.addEventListener('DOMContentLoaded', init);
 
-    payload["url"] = window.location.href;
+function init() {
+  let payload = {};
 
-    payload["timestamp"] = new Date().toISOString();
+  payload.url = window.location.href;
+  payload.timestamp = new Date().toISOString();
+  payload.userAgent = navigator.userAgent;
+  payload.screenDimensions = {
+    width: window.screen.width,
+    height: window.screen.height,
+  };
 
-    payload["userAgent"] = navigator.userAgent;
-    payload["screenDimensions"] = {
-        width: window.screen.width,
-        height: window.screen.height
-    };
+  const data = JSON.stringify(payload);
 
-    navigator.sendBeacon("https://cse135spain.site/api/static", JSON.stringify(payload));
+  fetch('https://cse135spain.site/api/static', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: data,
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Data sent successfully.');
+      } else {
+        console.log('Failed to send data.');
+      }
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
 }

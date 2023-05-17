8d6ae3d42312715d5959e0f1258fca06cdc4f090
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-16 23:16:16 -0700

    update

--- collector.js
+++ collector.js
@@ -2,7 +2,15 @@ window.addEventListener('DOMContentLoaded',init);
 function init(){
     let payload = {};
 
-    payload["url"] = window.location.href;;
+    payload["url"] = window.location.href;
+
+    payload["timestamp"] = new Date().toISOString();
+
+    payload["userAgent"] = navigator.userAgent;
+    payload["screenDimensions"] = {
+        width: window.screen.width,
+        height: window.screen.height
+    };
 
     navigator.sendBeacon("https://cse135spain.site/api/static", JSON.stringify(payload));
 }

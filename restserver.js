1e0eba0e72cdbdfc5fc2ad08da6704cda052c4cc
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-16 23:22:41 -0700

    Update restserver.js

--- restserver.js
+++ restserver.js
@@ -1,54 +1,61 @@
+const PORT = 3001;
+
 const express = require('express');
 const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { time } = require('console');
 
+// Create express app
 const app = express();
-const port = 3001;
 
-app.use(express.json());
+// Use body-parser middleware to parse request bodies
+app.use(bodyParser.json());
 
-// Create a connection pool
+// Better set up CORS properly or not depending on security posture
+app.use(cors({
+    origin: '*'
+}));
+
+// Create connection pool to MySQL database
+
 const pool = mysql.createPool({
-  host: '143.198.66.79',
-  user: 'root',
-  password: 'cse135',
-  database: 'rest',
+    host: '143.198.66.79',
+    user: 'sammy',
+    password: 'cse135',
+    database: 'rest'
 });
 
-// Helper function to execute SQL queries using the connection pool
-const executeQuery = async (query, values) => {
-  const connection = await pool.getConnection();
-  try {
-    const [rows, fields] = await connection.query(query, values);
-    return rows;
-  } finally {
-    connection.release();
-  }
-};
+// Enable "promise" for mysql2
+const promisePool = pool.promise();
+
+app.get('/static/', async (req, res) => {
+    const logs = require('./logs.json');
+  
+    res.json(logs);
+});
 
 app.post('/static/', async (req, res) => {
-  try {
-    const { url, timestamp, userAgent, screenDimensions } = req.body;
+    try {
+        const { url, timestamp, userAgent, screenDimensions } = req.body;
 
-    // Perform validation
-    if (!url || !timestamp || !userAgent || !screenDimensions) {
-      return res.status(400).send('Missing required information');
+        if (!url || !timestamp || !userAgent || !screenDimensions) {
+            console.log('Request Payload:', req.body)
+            return res.status(400).send('Missing info');
+        }
+
+        // Execute SQL query
+        const [result] = await promisePool.query('INSERT INTO users (url, timestamp, userAgent, screenDimensions) VALUES (?, ?, ?, ?)', [url, timestamp, userAgent, screenDimensions]);
+
+        // Send response
+        res.status(201).send(`User added with ID: ${result.insertId}`);
+    } catch (error) {
+        // Handle error
+        res.status(500).send(error);
     }
-
-    // Execute SQL query to insert data into the database
-    const query = 'INSERT INTO users (url, timestamp, userAgent, screenDimensions) VALUES (?, ?, ?, ?)';
-    const values = [url, timestamp, userAgent, JSON.stringify(screenDimensions)];
-
-    await executeQuery(query, values);
-
-    // Send response
-    res.status(201).send('User added successfully');
-  } catch (error) {
-    // Handle error
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });

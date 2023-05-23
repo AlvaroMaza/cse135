be242aecb4f0f9f158c38b27c2ee21aa4322a295
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-23 14:19:27 -0700

    Update restserver.js

--- restserver.js
+++ restserver.js
@@ -59,17 +59,47 @@ app.get('/static/:id', (req, res) => {
 
 // Add a new entry to the static table
 app.post('/static/', (req, res) => {
-  const { url, timestamp, userAgent, screenDimensions } = req.body;
-  if (!url || !timestamp || !userAgent || !screenDimensions) {
+  const {
+    url,
+    timestamp,
+    userAgent,
+    language,
+    cookieEnabled,
+    jsEnabled,
+    imagesEnabled,
+    cssEnabled,
+    screenDimensions,
+    windowDimensions,
+    connectionType
+  } = req.body;
+
+  if (!url || !timestamp || !userAgent || !language || 
+      typeof cookieEnabled !== 'boolean' ||
+      typeof jsEnabled !== 'boolean' ||
+      typeof imagesEnabled !== 'boolean' ||
+      typeof cssEnabled !== 'boolean' ||
+      !screenDimensions || !windowDimensions || !connectionType) {
     console.log('Request Payload:', req.body);
-    return res.status(400).send('Missing information');
+    return res.status(400).send('Missing or invalid information');
   }
 
   const formattedTimestamp = new Date(timestamp).toISOString().slice(0, 19).replace('T', ' ');
 
   connection.query(
-    'INSERT INTO static (url, timestamp, userAgent, screenDimensions) VALUES (?, ?, ?, ?)',
-    [url, formattedTimestamp, userAgent, JSON.stringify(screenDimensions)],
+    'INSERT INTO static (url, timestamp, userAgent, language, cookieEnabled, jsEnabled, imagesEnabled, cssEnabled, screenDimensions, windowDimensions, connectionType) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    [
+      url,
+      formattedTimestamp,
+      userAgent,
+      language,
+      cookieEnabled,
+      jsEnabled,
+      imagesEnabled,
+      cssEnabled,
+      JSON.stringify(screenDimensions),
+      JSON.stringify(windowDimensions),
+      connectionType
+    ],
     (error, results) => {
       if (error) {
         console.error('Error:', error);
@@ -97,14 +127,48 @@ app.delete('/static/:id', (req, res) => {
 
 // Update a specific entry from the static table (that matches the given id)
 app.put('/static/:id', (req, res) => {
-  const { url, timestamp, userAgent, screenDimensions } = req.body;
-  if (!url || !timestamp || !userAgent || !screenDimensions) {
+  const {
+    url,
+    timestamp,
+    userAgent,
+    language,
+    cookieEnabled,
+    jsEnabled,
+    imagesEnabled,
+    cssEnabled,
+    screenDimensions,
+    windowDimensions,
+    connectionType
+  } = req.body;
+
+  if (!url || !timestamp || !userAgent || !language || 
+      typeof cookieEnabled !== 'boolean' ||
+      typeof jsEnabled !== 'boolean' ||
+      typeof imagesEnabled !== 'boolean' ||
+      typeof cssEnabled !== 'boolean' ||
+      !screenDimensions || !windowDimensions || !connectionType) {
     console.log('Request Payload:', req.body);
-    return res.status(400).send('Missing information');
+    return res.status(400).send('Missing or invalid information');
   }
+
+  const formattedTimestamp = new Date(timestamp).toISOString().slice(0, 19).replace('T', ' ');
+
   connection.query(
-    'UPDATE static SET url = ?, timestamp = ?, userAgent = ?, screenDimensions = ? WHERE id = ?',
-    [url, timestamp, userAgent, JSON.stringify(screenDimensions), req.params.id],
+    'UPDATE static SET url = ?, timestamp = ?, userAgent = ?, language = ?, cookieEnabled = ?, jsEnabled = ?, imagesEnabled = ?, cssEnabled = ?, screenDimensions = ?, windowDimensions = ?, connectionType = ? WHERE id = ?',
+    [
+      url,
+      formattedTimestamp,
+      userAgent,
+      language,
+      cookieEnabled,
+      jsEnabled,
+      imagesEnabled,
+      cssEnabled,
+      JSON.stringify(screenDimensions),
+      JSON.stringify(windowDimensions),
+      connectionType,
+      req.params.id
+    ],
     (error, results) => {
       if (error) {
         console.error('Error:', error);
@@ -117,7 +181,6 @@ app.put('/static/:id', (req, res) => {
     }
   );
 });
-
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

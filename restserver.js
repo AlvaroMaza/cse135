0eda2502c0ffe5c89d4eb4400e7ee0b6fb02596e
Juan Villanueva <juanvill175@gmail.com>
2023-05-23 12:31:00 -0700

    Update restserver.js

--- restserver.js
+++ restserver.js
@@ -16,7 +16,7 @@ app.use(cors({
 // Create connection pool to MySQL database
 const pool = mysql.createPool({
   port: '/var/run/mysqld/mysqld.sock',
-  host: '143.198.66.79',
+  host: 'localhost',
   user: 'sammy',
   password: 'realmadrid',
   database: 'rest'

c64847d8a6fc3675b2bc627b576446909c442919
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-23 23:31:44 -0700

    Update dataviz.php

--- dataviz.php
+++ dataviz.php
@@ -1,58 +1,65 @@
-<?php
-$mysqli = new mysqli("localhost", "sammy", "realmadrid", "rest");
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
+</head>
+<body>
+  <div id="myChart"></div>
 
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-$data = mysqli_query($mysqli, "SELECT * FROM static");
-
-/* Create JavaScript objects from the data */
-?>
-
-<script>
-var myData = [
   <?php
-  while ($info = mysqli_fetch_array($data)) {
-      echo $info['f_data'] . ',';
+  $mysqli = new mysqli("localhost", "sammy", "realmadrid", "rest");
+
+  if (mysqli_connect_errno()) {
+      printf("Connect failed: %s\n", mysqli_connect_error());
+      exit();
   }
-  ?>
-];
 
-var myLabels = [
-  <?php
   $data = mysqli_query($mysqli, "SELECT * FROM static");
-  while ($info = mysqli_fetch_array($data)) {
-      echo '"' . $info['f_name'] . '",';
-  }
   ?>
-];
-</script>
 
-<?php
-$mysqli->close();
-?>
-
-<script>
-// Use the myData and myLabels arrays
-window.addEventListener('load', function() {
-  zingchart.render({
-    id: "myChart",
-    width: "100%",
-    height: 400,
-    data: {
-      type: 'bar',
-      title: {
-        text: "Data Pulled from MySQL Database"
-      },
-      'scale-x': {
-        labels: myLabels
-      },
-      series: [{
-        values: myData
-      }]
+  <script>
+  var myData = [
+    <?php
+    while ($info = mysqli_fetch_array($data)) {
+        echo $info['f_data'] . ',';
     }
+    ?>
+  ];
+
+  var myLabels = [
+    <?php
+    $data = mysqli_query($mysqli, "SELECT * FROM static");
+    while ($info = mysqli_fetch_array($data)) {
+        echo '"' . $info['f_name'] . '",';
+    }
+    ?>
+  ];
+  </script>
+
+  <?php
+  $mysqli->close();
+  ?>
+
+  <script>
+  window.addEventListener('load', function() {
+    zingchart.render({
+      id: "myChart",
+      width: "100%",
+      height: 400,
+      data: {
+        type: 'bar',
+        title: {
+          text: "Data Pulled from MySQL Database"
+        },
+        'scale-x': {
+          labels: myLabels
+        },
+        series: [{
+          values: myData
+        }]
+      }
+    });
   });
-});
-</script>
+  </script>
+</body>
+</html>

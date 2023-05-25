29927748d7a0cae2232a04aa7eb83eceb4ee68cb
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-24 18:31:29 -0700

    Update dataviz.php

--- dataviz.php
+++ dataviz.php
@@ -37,6 +37,16 @@
     }
     ?>
   ];
+
+  var langData = [
+    <?php
+    $langdata = mysqli_query($mysqli, "SELECT Language,COUNT(*) FROM performance GROUP BY Language");
+    echo $langdata
+    while ($langinfo = mysqli_fetch_array($langdata)) {
+        #echo '"' . $langinfo['loadStartTime'] . '",';
+    }
+    ?>
+  ];
   </script>
 
   <?php

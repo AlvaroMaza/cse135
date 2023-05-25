1c27c3f0f46f75e24c2d2cb294dc82d3e4bcf8bc
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-24 22:06:04 -0700

    Update dataviz.php

--- dataviz.php
+++ dataviz.php
@@ -244,11 +244,11 @@
       data: {
       type: "line",
       series: [{
-        "values": [20, 40, 25, 50, 15, 45, 33, 34]
+        "values": shiftData
       }, {
-        "values": [5, 30, 21, 18, 59, 50, 28, 33]
+        "values": ctrlData
       }, {
-        "values": [30, 5, 18, 21, 33, 41, 29, 15]
+        "values": altData
       }]
     }
     });

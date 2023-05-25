df35d71014ec19fa873b1153eb35a4c8310e21d1
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-24 19:05:40 -0700

    Update dataviz.php

--- dataviz.php
+++ dataviz.php
@@ -53,7 +53,7 @@
     $langdata = mysqli_query($mysqli, "SELECT Language,COUNT(*) FROM static GROUP BY Language");
 
     while ($langinfo = mysqli_fetch_array($langdata)) {
-          echo  $langinfo['Language'] . ',';
+          echo  '"' . $langinfo['Language'] . '",';
     }
     ?>
   ];

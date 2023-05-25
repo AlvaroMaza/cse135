881fa321b8e42a7ca27e707e3a6e15ac010e310e
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-05-24 19:03:42 -0700

    Update dataviz.php

--- dataviz.php
+++ dataviz.php
@@ -48,6 +48,16 @@
     ?>
   ];
 
+  var langLabels = [
+    <?php
+    $langdata = mysqli_query($mysqli, "SELECT Language,COUNT(*) FROM static GROUP BY Language");
+
+    while ($langinfo = mysqli_fetch_array($langdata)) {
+          echo  $langinfo['Language'] . ',';
+    }
+    ?>
+  ];
+
   </script>
 
   <?php

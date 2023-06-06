da0c3b71be971224018b4417899762544aea5c74
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-06-05 19:55:32 -0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 
+router.use(cors());
 
 // Read all users
 router.get("/db", async (req, res) => {

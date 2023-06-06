9635abd6e7029ccf3f336ae7af53eeb66c336f69
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-06-05 21:31:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,36 @@ app.get("/db", async (req, res) => {
   }
 });
 
+// Create a user
+app.post("/db", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update a user
+app.put("/db/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a user
+app.delete("/db/:id", async (req, res) => {
+  try {
+    await User.findByIdAndRemove(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.use("/user", user);
 

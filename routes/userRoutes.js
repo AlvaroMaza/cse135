790a404e28b85afa4374c10d0a59ba1067954aa8
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-06-05 19:29:06 -0700

    mongo to crud

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -86,4 +86,14 @@ router.get("/secure-api", auth, async (req, res) => {
     }
 });
 
+// Read all users
+router.get("/", async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
 module.exports = router;

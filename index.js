740f7b30fcaa65a6f4a260cca0a24063743f5fbf
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-06-07 22:43:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ app.get("/db", async (req, res) => {
   }
 });
 
+app.get("/db/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Create a user
 app.post("/db", async (req, res) => {
   try {

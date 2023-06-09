1f1a0af83063e5114d1bc7b91bc986b305e29e36
AlvaroMaza <alvaromazamontesinos@gmail.com>
2023-06-08 20:25:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -74,8 +74,8 @@
   <script>
   window.addEventListener('load', function() {
 
-    var width = 800;
-    var height = 400;
+    var width = 4000;
+    var height = 2000;
     var margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
     var svg = d3.select("#heatmap")
